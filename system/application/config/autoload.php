57451ffb725ba17b66363121701bd5cad3d5b38d
MohamamadReza <mark@mark-desktop.(none)>
2010-01-31 16:49:55 +0330

    add datamapper to project

--- system/application/config/autoload.php
+++ system/application/config/autoload.php
@@ -39,7 +39,7 @@
 |	$autoload['libraries'] = array('database', 'session', 'xmlrpc');
 */
 
-$autoload['libraries'] = array('MainController', 'database');
+$autoload['libraries'] = array('MainController', 'database', 'datamapper');
 
 
 /*
